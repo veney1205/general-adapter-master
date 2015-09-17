f02aed091c91a13e03f483a33f93cb321426b4ba
guodan <gdw117@163.com>
2015-09-17 15:05:26 +0800

    first commit

---
+++ app/src/main/java/com/example/guodan/myapplication/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.guodan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        User u = new User();
+        u.setId(1l);
+        u.setName("dd");
+        Group gp = new Group();
+        ArrayList<User> users = new ArrayList<>( );
+        users.add(u);
+        gp.setUsers(users);
+        gp.setName("22");
+        String str = JSON.toJSONString(gp);
+        System.out.println(str);
+        Group g = JSONObject.parseObject(str,Group.class);
+        System.out.print(g.getName());
+        System.out.println("2");
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class User {
+        private Long id;
+        private String name;
+
+        public Long getId() { return id; }
+        public void setId(Long id) { this.id = id; }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+    }
+
+    public class Group {
+        private Long id;
+        private String name;
+        private ArrayList<User> users = new ArrayList<>();
+
+        public Long getId() { return id; }
+        public void setId(Long id) { this.id = id; }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+
+        public ArrayList<User> getUsers() { return users; }
+        public void setUsers(ArrayList<User>users) { this.users = users; }
+    }
+}
